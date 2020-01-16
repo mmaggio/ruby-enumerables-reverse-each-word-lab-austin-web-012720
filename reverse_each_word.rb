c10041371009a1890a20fdfe2cddcd9fdf7943dc
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-16 21:23:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,9 +12,8 @@ end
 def reverse_each_word(string)
   reversed_words = []
   words = string.split(/ /)
-  words.collect do
+  reversed_words = words.collect do
     |n| n.reverse! 
-    reversed_words << n
   end
   reversed_sentence = reversed_words.join(' ')
   p reversed_sentence
